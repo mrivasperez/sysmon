8e360f5e8b5157155ca00db49d36f4c523412f7a
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-07-06 22:21:51 -0700

    updated styling

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,24 @@ const path = require("path"),
   mem = osu.mem,
   os = osu.os;
 
+// Change Darwin to macS in System Info
+const getOSInfo = () => {
+  if (os.type() == "Darwin") {
+    return "macOS";
+  }
+  return os.type();
+};
+
+// Show days, hours, mins, seconds
+const secondsToDhms = (seconds) => {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+};
+
 // *** DYNAMIC SYSTEM STATISTICS ***
 // *** Run every 2 seconds
 setInterval(() => {
@@ -11,6 +29,12 @@ setInterval(() => {
   cpu.usage().then((info) => {
     document.getElementById("cpu-usage").innerText = `${info}%`;
   });
+  // CPU Free
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = `${info}%`;
+  });
+  // Get system uptime
+  document.getElementById("sys-uptime").innerText = secondsToDhms(os.uptime());
 }, 2000);
 
 /// *** END ***
@@ -21,12 +45,6 @@ document.getElementById("cpu-model").innerHTML = cpu.model();
 // Display computer name
 document.getElementById("comp-name").innerHTML = os.hostname();
 // Display OS info
-const getOSInfo = () => {
-  if (os.type() == "Darwin") {
-    return "macOS";
-  }
-  return os.type();
-};
 document.getElementById("os").innerText = `${getOSInfo()} ${os.arch()}`;
 // total RAM
 mem.info().then((info) => {
