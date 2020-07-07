ebfdb25666cc6c86ed0312d88ee3aae775fc222c
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-07-06 21:48:44 -0700

    finished cpu monitoring functionality

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,7 +4,34 @@ const path = require("path"),
   mem = osu.mem,
   os = osu.os;
 
+// *** DYNAMIC SYSTEM STATISTICS ***
+// *** Run every 2 seconds
+setInterval(() => {
+  // CPU Usage
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = `${info}%`;
+  });
+}, 2000);
+
+/// *** END ***
+
+// *** STATIC INFORMATION ***
 //display CPU model
 document.getElementById("cpu-model").innerHTML = cpu.model();
-// dispaly computer name
+// Display computer name
 document.getElementById("comp-name").innerHTML = os.hostname();
+// Display OS info
+const getOSInfo = () => {
+  if (os.type() == "Darwin") {
+    return "macOS";
+  }
+  return os.type();
+};
+document.getElementById("os").innerText = `${getOSInfo()} ${os.arch()}`;
+// total RAM
+mem.info().then((info) => {
+  document.getElementById("mem-total").innerText = `${
+    info.totalMemMb / 1000
+  } GB`;
+});
+/// *** END ***
