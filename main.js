ae93c57e0fb3f302b090862d622aedee5cd2f2ae
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-07-18 22:49:56 -0700

    improved tray functionality

--- main.js
+++ main.js
@@ -55,6 +55,15 @@ app.on("ready", () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
+  // Prevent application from breaking when it is closed
+  mainWindow.on("close", (e) => {
+    if (!app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+    return true;
+  });
+
   const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
 
   // Create the tray icon
@@ -67,7 +76,18 @@ app.on("ready", () => {
     }
   });
 
-  mainWindow.on("ready", () => (mainWindow = null));
+  tray.on("right-click", (e) => {
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: "Quit",
+        click: () => {
+          app.isQuitting = true;
+          app.quit();
+        },
+      },
+    ]);
+    tray.popUpContextMenu(contextMenu);
+  });
 });
 
 const menu = [
